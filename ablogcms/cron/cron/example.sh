2526282941f1700284fc024291a62537ec270c00
uidev1116 <uidev1116@gmail.com>
2023-08-20 02:50:24 +0900

    不要なファイルを削除

--- ablogcms/cron/cron/example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export PATH=$PATH:/usr/local/bin
-cd $(dirname $0)
-php -d max_execution_time=0 -d memory_limit=512M -f example.php
-
-exit $?
--- ablogcms/themes/uidev/src/js/pretty-scroll.js
+++ ablogcms/themes/uidev/src/js/pretty-scroll.js
@@ -4,10 +4,10 @@ import PrettyScroll from 'pretty-scroll'
 export default () => {
   domContentLoaded(() => {
     // eslint-disable-next-line no-new
-    new PrettyScroll('.js-pretty-scroll', {
-      container: '.js-container',
-      breakpoint: 960,
-      offsetTop: 24,
-    })
+    // new PrettyScroll('.js-pretty-scroll', {
+    //   container: '.js-container',
+    //   breakpoint: 960,
+    //   offsetTop: 24,
+    // })
   })
 }
