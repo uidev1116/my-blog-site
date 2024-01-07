dcf1b29e78539c08266c469b4f7e8d6a3b2e25a3
uidev1116 <uidev1116@gmail.com>
2024-01-07 16:43:02 +0900

    build:git submoculeにApiPreview拡張アプリを追加

--- tools/setup.js
+++ tools/setup.js
@@ -1,17 +1,29 @@
-'use strict';
+'use strict'
 
-const { systemCmd } = require('./lib/system.js');
+const { systemCmd } = require('./lib/system.js')
 
-(async () => {
+const plugins = ['V2', 'ApiPreview']
+
+;(async () => {
   try {
-    await systemCmd('git submodule init');
-    await systemCmd('git submodule update');
-    await systemCmd('git submodule foreach npm ci');
-    await systemCmd('git submodule foreach npm run setup');
-    await systemCmd('npm ci');
-    await systemCmd('unlink ablogcms/extension/plugins/V2');
-    await systemCmd('ln -s ../../../plugins/v2/src ablogcms/extension/plugins/V2');
+    await systemCmd('git submodule init')
+    await systemCmd('git submodule update')
+    await systemCmd('git submodule foreach npm ci')
+    await systemCmd('git submodule foreach npm run setup')
+    await systemCmd('npm ci')
+    await Promise.all(
+      plugins.map((plugin) =>
+        systemCmd(`unlink ablogcms/extension/plugins/${plugin}`),
+      ),
+    )
+    await Promise.all(
+      plugins.map((plugin) =>
+        systemCmd(
+          `ln -s ../../../plugins/${plugin}/src ablogcms/extension/plugins/${plugin}`,
+        ),
+      ),
+    )
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-})();
+})()
